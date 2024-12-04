e053c4853cba766a420c02dbf652877df74ff7b8
BugHunter-X <askzenai@gmail.com>
2024-12-04 11:08:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,6 @@
+```javascript
+<div class="bg-gradient-to-r from-blue-500 to-purple-500 p-4 rounded-lg shadow-lg">
+  <h2 class="text-white text-2xl font-bold">Gradient Background</h2>
+  <p class="text-white mt-2">This is an example of a gradient background.</p>
+</div>
+```
